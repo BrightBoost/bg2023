e8af94eef0d9d9e0806326e08539182f7cb6a905
Maaike <maaikejvp@hotmail.com>
2023-11-20 11:53:02 +0100

    js variables

---
+++ week2/variables.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var nameDog; // declare 
+nameDog = "Bobby"; // initialize
+
+var age = 2; // declare and initialize
+
+var cup, notebook, tshirt; // declare multiple variables on the same line
+var audioInterface = "with fun effects", keyboard = "the loud one", phone = "Samsung"; // declare and initialized multiple variables on the same line
+
+
+var isGoodBoy = true;
+var sum = 2 + 4;
+var noObj = null;
+
+console.log(nameDog, "is", age, "year old.");
+
+console.log(typeof noObj);
+
+// funny JS stuff
+var nr1 = "hoi1";
+var nr2 = "2";
+var nr3 = nr1 * nr2;
+console.log("total:", nr3);
+console.log(typeof nr3);
