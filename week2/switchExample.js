a7d9fbbea8aa0fc9cabc42cbb26e9b10e5feb57f
Maaike <maaikejvp@hotmail.com>
2023-11-20 16:51:49 +0100

    switch

---
+++ week2/switchExample.js
@@ -0,0 +1,21 @@
+let day = "Christmas day";
+
+switch (day) {
+    case "Monday":
+        console.log("I hate Mondays");
+        break;
+    case "Tuesday":
+        console.log("It's Tuesday!");
+        break;
+    case "Wednesday":
+        console.log("This is Wednesday.");
+        break;
+    case "Thursday":
+        console.log("It's almost Friday.");
+        break;
+    case "Friday":
+        console.log("It's almost weekend!");
+        break;
+    default:
+        console.log("I don't know that day, but it sounds like fun. Have a nice ", day);
+}
