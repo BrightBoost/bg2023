9bcd50306486178327f42a72fdca9e84ab8d6fe3
Maaike <maaikejvp@hotmail.com>
2023-11-21 14:24:22 +0100

    functions

--- week2/basicEventHandling/scripts/script.js
+++ week2/basicEventHandling/scripts/script.js
@@ -7,6 +7,9 @@ function init() {
     const hiBtn = document.getElementById("sayHi");
     const byeBtn = document.getElementById("sayBye");
 
+    // little experiment
+    document.getElementById("readonlyTextBox").value = "blablabla";
+
     // set the click events
     hiBtn.onclick = hello;
     byeBtn.onclick = goodbye;
---
+++ week2/functionExamples.js
@@ -0,0 +1,8 @@
+function addTwoNumbers(a, b) {
+    console.log("In function:", a + b);
+    return a + b;
+}
+
+
+let finalResult = addTwoNumbers();
+console.log(finalResult);
