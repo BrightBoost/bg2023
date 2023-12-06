2602ff3322f5b8beeacb3915fc4bd0d94a0df25e
Maaike <maaikejvp@hotmail.com>
2023-12-06 11:07:57 +0100

    routing recap

--- week4/angularExamples/extra-recap/src/app/app.module.ts
+++ week4/angularExamples/extra-recap/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+
+const appRoutes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "product", component: ProductComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
