a85e1e5654f058d97bd45847fb52f23997932eae
Maaike <maaikejvp@hotmail.com>
2023-12-06 10:35:00 +0100

    zoe recap

---
+++ week4/angularExamples/extra-recap/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'extra-recap';
+  name = "Zoe";
+  example: string = "";
+
+  sayHi() {
+    alert("hi!");
+  }
+
+  changeExample(text: string) {
+    this.example = text;
+  }
+}
