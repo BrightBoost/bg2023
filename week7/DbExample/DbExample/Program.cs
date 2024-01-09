05b716a63829b66055d31a6a9dbdea7b55bec6fb
Maaike <maaikejvp@hotmail.com>
2024-01-09 17:21:00 +0100

    ef

---
+++ week7/DbExample/DbExample/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using DbExample.Models;
+
+namespace DbExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindContext context = new NorthwindContext();
+            var categories = context.Categories.Where(c => c.CategoryName.StartsWith("B"));
+            foreach( var category in categories )
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+    }
+}
