0ca3bfce9f38bb0a258606d0351a4d2c0bceef25
Maaike <maaikejvp@hotmail.com>
2024-01-10 11:01:11 +0100

    code first

---
+++ week7/CodeFirstDemo/CodeFirstDemo/Models/MyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstDemo.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EFCodeFirstBG;Integrated Security=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>()
+                .HasMany(author => author.Books)
+                .WithOne(book => book.Author);
+        }
+    }
+
+}
+
---
+++ week7/EFNorthwindSearch/EFNorthwindSearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using EFNorthwindSearch.Models;
+
+namespace EFNorthwindSearch
+{
+    internal class Program
+    {
+        static NorthwindContext northwindContext = new NorthwindContext();
+
+        static void Main(string[] args)
+        {
+            PromptingUserForCategory();
+        }
+
+        static void PromptingUserForCategory()
+        {
+            Console.WriteLine("What category do you want?");
+            DisplayAllCategories();
+            string category = Console.ReadLine();
+            DisplayProductsInCategory(category);
+        }
+
+        static void DisplayAllCategories()
+        {
+            foreach(var category in northwindContext.Categories)
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        static void DisplayProductsInCategory(string category)
+        {
+
+            var products = northwindContext.Categories
+                .Where(c => c.CategoryName == category)
+                .SelectMany(c => c.Products);
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.ProductName + " Unitprice " + product.UnitPrice);
+            }
+
+        }
+    }
+}
