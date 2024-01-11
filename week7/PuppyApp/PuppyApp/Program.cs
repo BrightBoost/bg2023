8351a1cb94f53693a2244548371434b26fc5b3ad
Maaike <maaikejvp@hotmail.com>
2024-01-11 14:22:09 +0100

    cors

--- week7/PuppyApp/PuppyApp/Program.cs
+++ week7/PuppyApp/PuppyApp/Program.cs
@@ -12,6 +12,16 @@ namespace PuppyApp
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("https://localhost:4200")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
