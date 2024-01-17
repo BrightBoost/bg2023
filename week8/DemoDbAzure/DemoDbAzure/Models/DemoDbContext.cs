e2f2a62d44728ff9439c7c4e4f62da236298a944
Maaike <maaikejvp@hotmail.com>
2024-01-17 17:41:17 +0100

    projects

---
+++ week8/DemoDbAzure/DemoDbAzure/Models/DemoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoDbAzure.Models
+{
+    public class DemoDbContext: DbContext
+    {
+        public DemoDbContext()
+        {
+
+        }
+
+        public DemoDbContext(DbContextOptions<DemoDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
